7e96ecb00def165822224babdc4140c990885bc0
Mikkel Thestrup <mikkel@mithe.dk>
2026-04-18 00:58:51 +0200

    added nix build

--- flake.nix
+++ flake.nix
@@ -1,54 +1,70 @@
 {
-    description = "dmenu";
-
-    inputs = {
-        nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-        flake-utils.url = "github:numtide/flake-utils";
-    };
-
-    outputs = { self, nixpkgs, flake-utils }:
-        flake-utils.lib.eachDefaultSystem (system:
-            let
-                pkgs = nixpkgs.legacyPackages.${system};
-            in
-                {
-                packages.default = pkgs.stdenv.mkDerivation {
-                    pname = "dmenu";
-                    version = "4.9";
-
-                    src = ./.;
-
-                    buildInputs = with pkgs; [
-                        xorg.libX11
-                        xorg.libXft
-                        xorg.libXinerama
-                        freetype
-                        fontconfig
-                    ];
-
-                    nativeBuildInputs = with pkgs; [
-                        pkg-config
-                    ];
-
-                    makeFlags = [ "PREFIX=$(out)" ];
-                };
-
-                devShells.default = pkgs.mkShell {
-                    buildInputs = with pkgs; [
-                        xorg.libX11
-                        xorg.libXft
-                        xorg.libXinerama
-                        freetype
-                        fontconfig
-                        pkg-config
-
-                        # Dev tools
-                        clang
-                        gcc
-                        gnumake
-                        gdb
-                    ];
-                };
-            }
-        );
+  description = "dmenu";
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    treefmt-nix.url = "github:numtide/treefmt-nix";
+  };
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    treefmt-nix,
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [
+            (final: prev: {
+              dmenu = prev.dmenu.overrideAttrs (_oldAttrs: {
+                version = "5.3";
+                src = ./.;
+              });
+            })
+          ];
+        };
+        treefmtEval = treefmt-nix.lib.evalModule pkgs {
+          projectRootFile = "flake.nix";
+          programs = {
+            alejandra.enable = true;
+            clang-format = {
+              enable = true;
+              includes = ["*.c" "*.h"];
+            };
+          };
+        };
+      in {
+        packages = {
+          dmenu = pkgs.dmenu;
+          default = pkgs.dmenu;
+        };
+        apps = {
+          dmenu = {
+            type = "app";
+            program = "${pkgs.dmenu}/bin/dmenu";
+          };
+          dmenu_run = {
+            type = "app";
+            program = "${pkgs.dmenu}/bin/dmenu_run";
+          };
+          default = {
+            type = "app";
+            program = "${pkgs.dmenu}/bin/dmenu";
+          };
+        };
+        formatter = treefmtEval.config.build.wrapper;
+        checks.formatting = treefmtEval.config.build.check self;
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            libx11
+            libxft
+            libxinerama
+            gcc
+            clang-tools
+            bear
+          ];
+        };
+      }
+    );
 }
